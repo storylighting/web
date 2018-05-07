f167446788e9f292da53b07063c056ae2255b53c
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2018-05-07 09:58:09 -0400

    Added Firestore Trigger

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,22 @@
 const functions = require('firebase-functions');
+var articleParsing = require('./articleParsing');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.makeArticleParagraphColors = functions.firestore.document('articles/{id}').onCreate((snapshot, context) => {
+  console.log("triggered");
+  // Grab the current value of what was written to the Realtime Database.
+  const article = snapshot.data();
+  console.log(article);
+  let paragraphs = article.paragraphs;
+  return articleParsing.articleSentenceToneAnalyzer(paragraphs)
+    .then(articleParsing.toneAnalysis)
+    .then(articleParsing.toneToColor)
+    .then(data => {
+      console.log(data);
+      return snapshot.ref.set({
+        colors: data
+      }, {merge: true});
+    })
+    .catch(err => {
+      console.log('failed\n', err);
+    });
+});
